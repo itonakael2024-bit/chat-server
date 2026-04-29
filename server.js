4bd3a2f590a111f1aecfb9e18327fc835a4d6e97
itonakael2024-bit <itonakael2024@gmail.com>
2026-04-30 00:13:46 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
-import express from "express";
-
-const app = express();
 app.use(express.json());
 
 app.post("/chat", async (req, res) => {
-  const message = req.body.message;
 
-  console.log("受信:", message);
+  console.log("受信データ:", req.body);
+
+  const messages = req.body.messages;
+
+  if (!messages) {
+    return res.json({
+      reply: "messagesがありません"
+    });
+  }
+
+  const lastMessage = messages[messages.length - 1].content;
 
   res.json({
-    reply: "テスト返信: " + message
+    reply: "テスト返信: " + lastMessage
   });
-});
-
-app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
-
-const PORT = process.env.PORT || 10000;
-
-app.listen(PORT, () => {
-  console.log("Server started on port", PORT);
 });
